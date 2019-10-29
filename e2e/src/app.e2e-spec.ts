23fae414ab2fcfccd5183398a061c8e68a90132f
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-29 09:41:15 -0500

    Se modifica nombre del proyecto

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to pwa!');
+    expect(page.getTitleText()).toEqual('Welcome to pwa-angular8!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pwa'`, () => {
+  it(`should have as title 'pwa-angular8'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('pwa');
+    expect(app.title).toEqual('pwa-angular8');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pwa!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pwa-angular8!');
   });
 });
